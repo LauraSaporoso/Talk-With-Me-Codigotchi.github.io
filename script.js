cca7c4fcd7f675470f98c1417987377ba0257400
LauraSaporoso <laurasaporoso99@gmail.com>
2022-12-15 19:34:58 +0100

    .

--- script.js
+++ script.js
@@ -13,10 +13,6 @@ mic.addEventListener('click', onStartListening);
 recog.addEventListener('result', onResult);
 
 
-/* ---------------------
-        FUNZIONI 
- ---------------------*/
-
 function onStartListening() {
     console.log('Start listening..');
     recog.start();
@@ -24,23 +20,16 @@ function onStartListening() {
 }
 
 function onResult(e) {
-    
     const testo = e.results[0][0].transcript;
     console.log(testo);
-
     const action = commands.find(function(commando) { 
         return testo.toLowerCase().includes(commando); 
     });
-
     const actionClassname = 'codigotchi-screen_' + action; 
-    screen.classList.add(actionClassname);
-
-    
-    panelData.classList.remove('listening');
-
-   
+    screen.classList.add(actionClassname); 
+    panelData.classList.remove('listening'); 
     setTimeout(function()
     {
-        screen.classList.remove(actionClassname);
+      screen.classList.remove(actionClassname);
     }, 3000);
 }
