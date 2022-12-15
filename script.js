3d2433169971eb93d941417ac4fb93449b6c691f
LauraSaporoso <laurasaporoso99@gmail.com>
2022-12-15 19:10:07 +0100

    all files

---
+++ script.js
@@ -0,0 +1,46 @@
+// condizione per far funzionare Speech..(comando esistente in javascript) in base al browser, alcuni hanno bisogno di webkit
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const mic = document.getElementById('microphone');
+const screen = document.getElementById('screen');
+const panelData = document.getElementById('panels-data');
+
+const commands = ['mangia', 'balla', 'dormi'];
+const recog = new SpeechRecognition();
+
+mic.addEventListener('click', onStartListening);
+ 
+recog.addEventListener('result', onResult);
+
+
+/* ---------------------
+        FUNZIONI 
+ ---------------------*/
+
+function onStartListening() {
+    console.log('Start listening..');
+    recog.start();
+    panelData.classList.add('listening');
+}
+
+function onResult(e) {
+    
+    const testo = e.results[0][0].transcript;
+    console.log(testo);
+
+    const action = commands.find(function(commando) { 
+        return testo.toLowerCase().includes(commando); 
+    });
+
+    const actionClassname = 'codigotchi-screen_' + action; 
+    screen.classList.add(actionClassname);
+
+    
+    panelData.classList.remove('listening');
+
+   
+    setTimeout(function()
+    {
+        screen.classList.remove(actionClassname);
+    }, 3000);
+}
